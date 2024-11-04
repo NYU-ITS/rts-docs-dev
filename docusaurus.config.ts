d01c9d8789fec8d93ac20907c5ec9d1b5546fd49
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2024-11-04 13:52:44 -0500

    CI:preview action: docasaurus specialization
    	modified:   .github/workflows/preview.yml
    	modified:   docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,13 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+// Set the /<baseUrl>/ pathname under which your site is served
+// For GitHub pages deployment, it is often '/<projectName>/'
+let baseUrl = '/rts-docs-dev/';
+if (process.env.PREVIEW_PATH) {
+  baseUrl += process.env.PREVIEW_PATH;
+}
+  
 const config: Config = {
   title: 'Research Technology Servcies',
   tagline: 'NYU',
@@ -11,7 +18,7 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/rts-docs-dev/',
+  baseUrl: baseUrl,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
