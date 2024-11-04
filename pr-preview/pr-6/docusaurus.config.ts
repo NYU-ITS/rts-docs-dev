3a9d86f2c4d8d8e6af7a47ce8bdd4afe4ebec661
s-sajid-ali <s-sajid-ali@users.noreply.github.com>
2024-11-04 19:05:44 +0000

    Deploy preview for PR 6 🛫

---
+++ pr-preview/pr-6/docusaurus.config.ts
@@ -0,0 +1,139 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// Set the /<baseUrl>/ pathname under which your site is served
+// For GitHub pages deployment, it is often '/<projectName>/'
+let baseUrl = '/rts-docs-dev/';
+if (process.env.PREVIEW_PATH) {
+  baseUrl += process.env.PREVIEW_PATH;
+}
+  
+const config: Config = {
+  title: 'Research Technology Servcies',
+  tagline: 'NYU',
+  favicon: 'img/NYU.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: baseUrl,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'NYU-ITS', // Usually your GitHub org/user name.
+  projectName: 'rts-docs-dev', // Usually your repo name.
+  deploymentBranch: 'main',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  //https://docusaurus.io/docs/deployment#docusaurusconfigjs-settings
+  trailingSlash: true,
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/NYU.svg',
+    navbar: {
+      title: 'Research Technology Services',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/NYU.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'right',
+          label: 'HPC',
+        },
+        { to: '/blog', label: 'Announcements', position: 'right' },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Feedback',
+          items: [
+            {
+              label: 'Email',
+              href: 'mailto:hpc@nyu.edu',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/NYU-ITS/rts-docs-dev',
+            },
+          ],
+        },
+      ],
+      copyright: `Built with Docusaurus!`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
